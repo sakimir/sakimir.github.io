f3a7218dc5fa8cf50b7940a13ccf770a93e55cea
sakimir <ppapingggg@gmail.com>
2023-06-07 12:11:16 +0800

    Site updated: 2023-06-07 12:11:15

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "50",
+        "fixed": "false",
+        "vertical": "center",
+        "horizontal": "center",
+        "key": "223ec8cc933547d3b9cf99bf2775bf20"
+    }
+}
